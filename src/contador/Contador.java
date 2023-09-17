2b533cdbf13e9f71b17f3b1165540c53abe97f95
Ivan Consalter <ivanconsalter1@gmail.com>
2023-09-17 17:58:36 -0300

    contador

---
+++ src/contador/Contador.java
@@ -0,0 +1,38 @@
+package contador;
+
+import java.util.Scanner;
+
+import contador.exception.ParametroInvalido;
+
+public class Contador {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		
+		System.out.print("Digite o primeiro parâmetro: ");
+		int parametroUm = terminal.nextInt();
+		
+		System.out.print("Digite o segundo parâmetro: ");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+		
+		} catch (ParametroInvalido e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
+		
+	}
+	
+	static void contar(int parametroUm, int parametroDois ) throws ParametroInvalido {
+        if(parametroUm > parametroDois) {
+        	throw new ParametroInvalido("O segundo parâmetro deve ser maior que o primeiro");
+        }
+		
+		int contagem = parametroDois - parametroUm;
+        for (int i = 0; i < contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+	}
+}
---
+++ src/contador/exception/ParametroInvalido.java
@@ -0,0 +1,10 @@
+package contador.exception;
+
+public class ParametroInvalido extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public ParametroInvalido(String message) {
+		super(message);
+	}
+}
